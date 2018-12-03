06aee228711b571768795e33ad98ca80514dd678
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-03 12:52:14 +0000

    fixed typo for tony/toney

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -4,7 +4,7 @@ describe BankAccount do
   describe '#Initialize' do
     it 'should list name, account number, balance' do
       bank_account = BankAccount.new('Tony', 101911)
-      expect(bank_account.owner).to eq('Toney')
+      expect(bank_account.owner).to eq('Tony')
       expect(bank_account.account_num).to eq(101911)
       expect(bank_account.balance).to eq(0)
     end
