c0a946e5b4bff327c2b4619b52f8add1190d8e35
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-03 13:48:56 +0000

    added account_num to init

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
-  attr_reader :owner
-  
+  attr_reader :owner, :account_num
+
   def initialize(name, account_num)
     @owner = name
+    @account_num = account_num
   end
 
 end
