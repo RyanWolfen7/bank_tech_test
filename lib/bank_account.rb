3f3e1a24cd7f68f1b0d898005c8139807cc9f9fe
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-04 13:59:36 +0000

    irb print complies with test

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 # This is the BankAccount class that handles users funds
 class BankAccount
-  DEFAULT_BALANCE = 0
+  DEFAULT_BALANCE = 0.00
   STATEMENT_TITLE_ARR = %w[Time Credit Debit Balance].freeze
 
   attr_reader :owner, :account_num, :balance, :statement
@@ -20,6 +20,12 @@ class BankAccount
   def withdraw(amount)
     @balance -= amount
     credit_statement(amount)
+  end
+
+  def print_statement
+    @statement.each do |item|
+      puts "#{item[0]} || #{item[1]} || #{item[2]} || #{item[3]}"
+    end
   end
 
   private
