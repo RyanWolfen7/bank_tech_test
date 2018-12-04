3fe452271cf610985213e6512b97cbd099820be7
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-04 11:20:24 +0000

    added spec helper for future refractor

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,4 +1,4 @@
-require 'bank_account.rb'
+require 'bank_account_helper.rb'
 
 describe BankAccount do
   describe '#Initialize' do
@@ -20,10 +20,20 @@ describe BankAccount do
     it 'should have an array of time, deposit, and balance' do
       bank_account = BankAccount.new('Tony', 101_911)
       bank_account.deposit(500)
-      expect(bank_account.statement[1][0]).to eq(Time.now.strftime '%d/%m/%Y')
+      expect(bank_account.statement[1][0]).to eq(Time.now.strftime('%d/%m/%Y'))
       expect(bank_account.statement[1][1]).to eq('')
       expect(bank_account.statement[1][2]).to eq(500)
       expect(bank_account.statement[1][3]).to eq(500)
+    end
+
+    it 'should have the correct balance in the second statement' do
+      bank_account = BankAccount.new('Tony', 101_911)
+      bank_account.deposit(500)
+      bank_account.deposit(500)
+      expect(bank_account.statement[2][0]).to eq(Time.now.strftime('%d/%m/%Y'))
+      expect(bank_account.statement[2][1]).to eq('')
+      expect(bank_account.statement[2][2]).to eq(500)
+      expect(bank_account.statement[2][3]).to eq(1000)
     end
   end
 
