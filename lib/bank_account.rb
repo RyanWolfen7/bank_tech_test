5a64e967c5503bd0a536e6a37cc857538d200777
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-04 11:07:17 +0000

    refactored deposit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,7 @@
 # This is the BankAccount class that handles users funds
 class BankAccount
   DEFAULT_BALANCE = 0
+  STATEMENT_TITLE_ARR = %w[Time Credit Debit Balance].freeze
 
   attr_reader :owner, :account_num, :balance, :statement
 
@@ -8,15 +9,21 @@ class BankAccount
     @owner = name
     @account_num = account_num
     @balance = DEFAULT_BALANCE
-    @statement = [['Time', 'Credit', 'Debit', 'Balance']]
+    @statement = [STATEMENT_TITLE_ARR]
   end
 
   def deposit(amount)
     @balance += amount
-    @statement << [Time.now.strftime('%d/%m/%Y'), '', amount, @balance]
+    deposit_statement(amount)
   end
 
   def withdraw(amount)
     @balance -= amount
+  end
+
+  private
+
+  def deposit_statement(amount)
+    @statement << [Time.now.strftime('%d/%m/%Y'), '', amount, @balance]
   end
 end
