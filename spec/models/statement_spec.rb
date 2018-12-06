ae94a2fa092896fa0d89d512ff077932bdaeb4a9
RyanWolfen7 <wolfen7lada@gmail.com>
2018-12-06 12:46:32 +0000

    updated spec to include reversal on print

--- spec/models/statement_spec.rb
+++ spec/models/statement_spec.rb
@@ -4,13 +4,13 @@ describe Statement do
   describe '#initialize' do
     it 'should hold an the default title' do
       statement = Statement.new
-      expect(statement.log).to eq([%w[Time Credit Debit Balance]])
+      expect(statement.log).to eq([])
     end
 
     it 'should hold a 2d array' do
       statement = Statement.new
       statement.log << ['june', '', 500, 500]
-      var = [%w[Time Credit Debit Balance], ['june', '', 500, 500]]
+      var = [['june', '', 500, 500]]
       expect(statement.log).to eq(var)
     end
   end
@@ -20,7 +20,7 @@ describe Statement do
       statement.log << statement = Statement.new
       statement.log << ['june', '', 500, 500]
       statement.log << ['june', 50, '', 450]
-      expect(statement.print_statement).to eq(statement.log)
+      expect(statement.print_statement).to eq(statement.log.reverse)
     end
   end
 end
